7a69149dcc8e12e83933aed56de2cb234a90de76
Eve <evejrobles@icloud.com>
2018-11-02 11:00:39 -0600

    added newlines

--- app/src/main/java/edu/cnm/deepdive/mobilepunch/view/MainActivity.java
+++ app/src/main/java/edu/cnm/deepdive/mobilepunch/view/MainActivity.java
@@ -2,21 +2,17 @@ package edu.cnm.deepdive.mobilepunch.view;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.NavigationView;
-import android.support.design.widget.Snackbar;
 import android.support.v4.app.Fragment;
 import android.support.v4.app.FragmentManager;
 import android.support.v4.app.FragmentTransaction;
 import android.support.v4.view.GravityCompat;
 import android.support.v4.widget.DrawerLayout;
-import android.support.v7.app.ActionBar;
 import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.Menu;
 import android.view.MenuItem;
-import android.view.View;
 import edu.cnm.deepdive.mobilepunch.R;
 
 public class MainActivity extends AppCompatActivity
