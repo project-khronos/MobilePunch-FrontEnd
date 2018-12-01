d6c2650750803afa487d5beb0952ae6b6874963b
Eve <evejrobles@icloud.com>
2018-12-01 15:57:46 -0700

    added date time picker

--- app/src/main/java/edu/cnm/deepdive/mobilepunch/model/dao/EventDao.java
+++ app/src/main/java/edu/cnm/deepdive/mobilepunch/model/dao/EventDao.java
@@ -1,8 +1,13 @@
 package edu.cnm.deepdive.mobilepunch.model.dao;
 
 import android.arch.persistence.room.Dao;
+import android.arch.persistence.room.Insert;
+import android.arch.persistence.room.OnConflictStrategy;
+import edu.cnm.deepdive.mobilepunch.model.entities.EventEntity;
 
 @Dao
 public interface EventDao {
+  @Insert(onConflict = OnConflictStrategy.FAIL)
+  long insert(EventEntity event);
 
 }
