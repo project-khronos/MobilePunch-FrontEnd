5c396fc5110734b88890a56083de54d53c95ecad
ToraKaji <ogradysjr@gmail.com>
2018-12-04 16:35:09 -0600

    Added directory

--- app/src/main/java/edu/cnm/deepdive/mobilepunch/model/entities/EquipmentEntity.java
+++ app/src/main/java/edu/cnm/deepdive/mobilepunch/model/entities/EquipmentEntity.java
@@ -87,6 +87,16 @@ public class EquipmentEntity {
     this.uuid = uuid;
   }
 
+  private String description;
+
+  public String getDescription() {
+    return description;
+  }
+
+  public void setDescription(String description) {
+    this.description = description;
+  }
+
   @NonNull
   public String getIdentification() {
     return Identification;
