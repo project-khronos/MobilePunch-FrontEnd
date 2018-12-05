d6fa3d95c9ba1811647f6da790064f9d3c428102
Eve <evejrobles@icloud.com>
2018-12-05 09:36:10 -0700

    started project adapter

--- app/src/main/java/edu/cnm/deepdive/mobilepunch/controller/ProjectRecyclerviewAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/mobilepunch/controller/ProjectRecyclerviewAdapter.java
@@ -7,6 +7,7 @@ import android.view.ViewGroup;
 
 public class ProjectRecyclerviewAdapter extends RecyclerView.Adapter {
 
+
   @NonNull
   @Override
   public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
