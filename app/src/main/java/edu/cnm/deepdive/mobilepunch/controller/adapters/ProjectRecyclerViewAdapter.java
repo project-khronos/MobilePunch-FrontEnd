57529b9622e70fce071987ecab3e98381465d78c
Eve <evejrobles@icloud.com>
2018-12-05 10:28:21 -0700

    project adapter

--- app/src/main/java/edu/cnm/deepdive/mobilepunch/controller/adapters/ProjectRecyclerViewAdapter.java
+++ app/src/main/java/edu/cnm/deepdive/mobilepunch/controller/adapters/ProjectRecyclerViewAdapter.java
@@ -1,26 +1,72 @@
 package edu.cnm.deepdive.mobilepunch.controller.adapters;
 
+import android.content.Context;
 import android.support.annotation.NonNull;
 import android.support.v7.widget.RecyclerView;
 import android.support.v7.widget.RecyclerView.ViewHolder;
+import android.view.LayoutInflater;
+import android.view.View;
 import android.view.ViewGroup;
+import android.widget.TextView;
+import edu.cnm.deepdive.mobilepunch.R;
+import edu.cnm.deepdive.mobilepunch.controller.adapters.EventRecyclerViewAdapter.ItemClickListener;
+import edu.cnm.deepdive.mobilepunch.model.entities.ProjectEntity;
+import java.util.List;
 
-public class ProjectRecyclerViewAdapter extends RecyclerView.Adapter {
+public class ProjectRecyclerViewAdapter extends
+    RecyclerView.Adapter<ProjectRecyclerViewAdapter.ProjectHolder> {
+
+  private List<ProjectEntity> data;
+  private LayoutInflater layoutInflater;
+  private ItemClickListener itemClickListener;
+  private Context context;
+
+  public ProjectRecyclerViewAdapter(Context context, List<ProjectEntity> data) {
+    this.layoutInflater = LayoutInflater.from(context);
+    this.data = data;
+    this.context = context;
+  }
 
 
   @NonNull
   @Override
-  public ViewHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
-    return null;
+  public ProjectHolder onCreateViewHolder(@NonNull ViewGroup viewGroup, int i) {
+    View view = layoutInflater.inflate(R.layout.project_list_item, viewGroup, false);
+    return new ProjectHolder(view);
   }
 
   @Override
-  public void onBindViewHolder(@NonNull ViewHolder viewHolder, int i) {
+  public void onBindViewHolder(@NonNull ProjectHolder projectHolder, int position) {
 
   }
+
 
   @Override
   public int getItemCount() {
     return 0;
+  }
+
+
+  public class ProjectHolder extends RecyclerView.ViewHolder implements View.OnClickListener{
+    private TextView name;
+    private TextView startDate;
+    private TextView endDate;
+    private TextView expectedEndDate;
+    private TextView description;
+
+
+    public ProjectHolder(@NonNull View itemView) {
+      super(itemView);
+      name = itemView.findViewById(R.id.li_project_name);
+      startDate = itemView.findViewById(R.id.li_project_start_date);
+      endDate = itemView.findViewById(R.id.li_project_end_date);
+      expectedEndDate = itemView.findViewById(R.id.li_project_expected);
+      description = itemView.findViewById(R.id.li_event_description);
+    }
+
+    @Override
+    public void onClick(View v) {
+
+    }
   }
 }
