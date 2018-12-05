667f2a05e646cdeee1e60b83ad87b1f437b825dc
Travis Decker <decker4693@gmail.com>
2018-12-04 23:48:20 -0700

    Added put methods to service class

--- app/src/main/java/edu/cnm/deepdive/mobilepunch/service/MobilePunchService.java
+++ app/src/main/java/edu/cnm/deepdive/mobilepunch/service/MobilePunchService.java
@@ -6,8 +6,11 @@ import edu.cnm.deepdive.mobilepunch.model.entities.ProjectEntity;
 import java.util.List;
 import okhttp3.ResponseBody;
 import retrofit2.Call;
+import retrofit2.Response;
+import retrofit2.http.Body;
 import retrofit2.http.GET;
 import retrofit2.http.Header;
+import retrofit2.http.PUT;
 
 
 public interface MobilePunchService {
@@ -15,6 +18,10 @@ public interface MobilePunchService {
   @GET("projects")
   Call<List<ProjectEntity>> getProjects(
       @Header("Authorization") String authorization);
+
+  @PUT("projects")
+  Response<List<ProjectEntity>> putProjects(
+      @Header("Authorization") String authorization, @Body List<ProjectEntity> projects);
 
   @GET("projects")
   Call<ResponseBody> getProjectsJson(
@@ -25,6 +32,10 @@ public interface MobilePunchService {
   Call<List<ClientEntity>> getClients(
       @Header("Authorization") String authorization);
 
+  @PUT("clients")
+  Response<List<ClientEntity>> putClients(
+      @Header("Authorization") String authorization, @Body List<ClientEntity> clients);
+
   @GET("clients")
   Call<ResponseBody> getClientsJson(
       @Header("Authorization") String authorization);
@@ -32,6 +43,10 @@ public interface MobilePunchService {
   @GET("equipment")
   Call<List<EquipmentEntity>> getEquipment(
       @Header("Authorization") String authorization);
+
+  @PUT("equipment")
+  Response<List<EquipmentEntity>> putEquipment(
+      @Header("Authorization") String authorization, @Body List<EquipmentEntity> equipmentList);
 
   @GET("equipment")
   Call<ResponseBody> getEquipmentJson(
