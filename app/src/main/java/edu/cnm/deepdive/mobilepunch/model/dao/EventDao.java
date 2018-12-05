4c6f7acc477c17adfade0995421c2aa28a8ceea3
ToraKaji <ogradysjr@gmail.com>
2018-12-05 09:31:44 -0600

    Merge remote-tracking branch 'origin/master'
    # Conflicts:
    #	app/src/main/java/edu/cnm/deepdive/mobilepunch/model/dao/EventDao.java
    #	app/src/main/java/edu/cnm/deepdive/mobilepunch/model/entities/EquipmentEntity.java
    #	app/src/main/java/edu/cnm/deepdive/mobilepunch/model/entities/EventEntity.java

--- app/src/main/java/edu/cnm/deepdive/mobilepunch/model/dao/EventDao.java
+++ app/src/main/java/edu/cnm/deepdive/mobilepunch/model/dao/EventDao.java
@@ -1,7 +1,6 @@
 package edu.cnm.deepdive.mobilepunch.model.dao;
 
 import android.arch.persistence.room.Dao;
-import android.arch.persistence.room.Entity;
 import android.arch.persistence.room.Insert;
 import android.arch.persistence.room.OnConflictStrategy;
 import android.arch.persistence.room.Query;
@@ -11,9 +10,14 @@ import java.util.List;
 @Dao
 public interface EventDao {
 
-  @Insert(onConflict = OnConflictStrategy.FAIL)
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
   long insert(EventEntity event);
 
-  @Query("Select * from EventEntity")
-  List<EventEntity> query();
+  @Insert(onConflict = OnConflictStrategy.REPLACE)
+  List<Long> insert(List<EventEntity> events);
+
+  @Query("SELECT * FROM EventEntity WHERE project_id1 = :project_id1 AND project_id2= :project_id2 ")
+  List<EventEntity> select(long project_id1, long project_id2);
+
+
 }
