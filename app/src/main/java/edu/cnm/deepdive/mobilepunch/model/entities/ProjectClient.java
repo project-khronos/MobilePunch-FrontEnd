9d99f3eb7ba41d03964323b5f67affb2f1a59146
Alex Benedict <20505631+abenedic@users.noreply.github.com>
2018-12-05 17:06:22 -0700

    Added a ProjectClient entity. Still need to add Dao's and add them to the database class.

---
+++ app/src/main/java/edu/cnm/deepdive/mobilepunch/model/entities/ProjectClient.java
@@ -0,0 +1,50 @@
+package edu.cnm.deepdive.mobilepunch.model.entities;
+
+import android.arch.persistence.room.Entity;
+
+@Entity
+public class ProjectClient {
+    private long projectId1;
+    private long projectId2;
+    private long clientId1;
+    private long clientId2;
+
+    public ProjectClient(ProjectEntity project, ClientEntity client) {
+        projectId1 = project.getId1();
+        projectId2 = project.getId2();
+        clientId1 = client.getId1();
+        clientId2 = client.getId2();
+    }
+
+    public long getProjectId1() {
+        return projectId1;
+    }
+
+    public void setProjectId1(long projectId1) {
+        this.projectId1 = projectId1;
+    }
+
+    public long getProjectId2() {
+        return projectId2;
+    }
+
+    public void setProjectId2(long projectId2) {
+        this.projectId2 = projectId2;
+    }
+
+    public long getClientId1() {
+        return clientId1;
+    }
+
+    public void setClientId1(long clientId1) {
+        this.clientId1 = clientId1;
+    }
+
+    public long getClientId2() {
+        return clientId2;
+    }
+
+    public void setClientId2(long clientId2) {
+        this.clientId2 = clientId2;
+    }
+}
